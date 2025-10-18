712d85a6aef9b9e83384484beb4d718136e7ee07
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2025-10-18 19:20:51 +0300

    Initial commit

---
+++ src/main/kotlin/org/project/wavelength/WaveLengthApplication.kt
@@ -0,0 +1,11 @@
+package org.project.wavelength
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WaveLengthApplication
+
+fun main(args: Array<String>) {
+    runApplication<WaveLengthApplication>(*args)
+}
